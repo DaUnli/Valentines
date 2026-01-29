0f30de2143c4f96e1a5d299a863dcd5184e47378
YAJE <gilmiercabil@gmail.com>
2026-01-29 10:14:04 +0800

    u

--- java.js
+++ java.js
@@ -1,7 +1,23 @@
-
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+function nextPage() {
+  window.location.href = "yes.html";
+}
+
+function moveButton() {
+  var x =
+    Math.random() *
+      (window.innerWidth - document.getElementById("noButton").offsetWidth) -
+    85;
+  var y =
+    Math.random() *
+      (window.innerHeight - document.getElementById("noButton").offsetHeight) -
+    48;
+  document.getElementById("noButton").style.left = `${x}px`;
+  document.getElementById("noButton").style.top = `${y}px`;
+}
